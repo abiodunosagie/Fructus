758058caa57ea701b98891a45c88f399cda8a1c4
Abiodun Osagie <abiodunosagie8@gmail>
2025-01-28 12:45:16 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Abiodun Osagie on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
