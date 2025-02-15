4e3ef11b4b52d242b74b67234f70ea83426056ff
Abiodun Osagie <abiodunosagie8@gmail>
2025-02-15 14:00:57 +0100

    Fruitus settings and ipad responsiveness feature.

---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Abiodun Osagie on 15/02/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ Fructus/View/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  Fructus
+//
+//  Created by Abiodun Osagie on 15/02/2025.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
